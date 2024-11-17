77bcc65b228d0a6c8fb85eac2b78199a061da1be
Doug Russell <doug@therussellfamily.org>
2024-11-17 13:13:02 -0500

    refactor: Refactored to clean things up a bit.

--- rust/embedded/STM32Discover_Kit-IoT_Node/blinky/src/main.rs
+++ rust/embedded/STM32Discover_Kit-IoT_Node/blinky/src/main.rs
@@ -3,15 +3,31 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use hal::{delay::Delay, pac, prelude::*};
 use panic_itm as _;
 use stm32l4 as _;
-use stm32l4xx_hal as hal;
+use stm32l4xx_hal::{
+    delay::Delay, gpio::Output, gpio::Pin, gpio::PushPull, gpio::L8, pac, prelude::*,
+};
 
 #[entry]
 fn main() -> ! {
-    // Take ownership of the stm32l475 device peripherals.
+    let (mut ld1, mut delay) = init();
+
+    let half_period = 500_u16;
+
+    loop {
+        ld1.toggle();
+        delay.delay_ms(half_period);
+    }
+}
+
+// The following would, most likely, be placed in a different module.
+type Led1 = Pin<Output<PushPull>, L8, 'A', 5>;
+
+fn init() -> (Led1, Delay) {
+    // Take ownership of the stm32l475 cortex m and device peripherals.
     let device_periphs = pac::Peripherals::take().unwrap();
+    let core_periphs = cortex_m::Peripherals::take().unwrap();
 
     // Constrain and configure the RCC (clock control) peripheral.
     let mut rcc = device_periphs.RCC.constrain();
@@ -23,19 +39,11 @@ fn main() -> ! {
 
     // Acquire GPIOA and configure PA5 as a push-pull output.
     let mut gpioa = device_periphs.GPIOA.split(&mut rcc.ahb2);
-    let mut ld1 = gpioa
+    let ld1 = gpioa
         .pa5
         .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
 
-    let half_period = 500_u16;
-    let core_periphs = cortex_m::Peripherals::take().unwrap();
-    let mut delay = Delay::new(core_periphs.SYST, clocks);
+    let delay = Delay::new(core_periphs.SYST, clocks);
 
-    loop {
-        ld1.set_high();
-        delay.delay_ms(half_period);
-
-        ld1.set_low();
-        delay.delay_ms(half_period);
-    }
+    (ld1, delay)
 }
